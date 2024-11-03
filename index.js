001e5f3e443c3d3e947a56bd8773c133bda16daf
RomeloDev <romelolucot1232@gmail.com>
2024-11-03 20:28:01 +0800

    Initial Commit:

---
+++ index.js
@@ -0,0 +1,19 @@
+function multiply(){
+    let firstInput = parseInt(prompt("Enter first number: "))
+    let secondInput = parseInt(prompt("Enter Second Number: "))
+    let result = firstInput * secondInput
+    console.log("Result: "+result)
+}
+
+function increment(){
+    let increInput = parseInt(prompt("Enter a number: "))
+    increInput++
+    console.log(increInput)
+}
+
+function compound(){
+    let num = parseInt(prompt("Enter a number: "))
+    num += 5
+    console.log(num)
+}
+  
